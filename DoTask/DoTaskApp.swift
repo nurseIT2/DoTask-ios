9492d7187747f19ee9f609db0855a08922741be4
nurseit <chocolatenutella0202@gmail.com>
2025-06-03 00:07:08 +0500

    Initial Commit

---
+++ DoTask/DoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTaskApp.swift
+//  DoTask
+//
+//  Created by Nurseit on 03.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct DoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DoTaskTests/DoTaskTests.swift
@@ -0,0 +1,16 @@
+//
+//  DoTaskTests.swift
+//  DoTaskTests
+//
+//  Created by Nurseit on 03.06.2025.
+//
+
+import Testing
+
+struct DoTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
